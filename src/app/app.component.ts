7299a1aa134750d80be4d9ca5aa1e0f87fed20d4
Mariam Reintop <mariam.reintop@tlu.ee>
2018-07-01 20:46:21 +0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root',/**This is the name to be used*/
+  templateUrl: './app.component.html', /**This is the components template*/
+  styleUrls: ['./app.component.scss']/**This is the style for this template*/
 })
 export class AppComponent {
+    /**This is the logic part*/
   title = 'app';
 }
